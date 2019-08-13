e093b58b5a5c47e4bb7dee570618f0991cf5953d
Erin Gurley <erinmgurley@gmail.com>
2019-08-13 16:45:46 -0700

    initialized

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ThumbnailGallery from './thumbnail-gallery';
+
+export default function App() {
+  return (
+    <div>Thumbnail Gallery</div>
+  );
+}
---
+++ src/components/thumbnail-gallery/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ThumbnailGallery extends Component {
+  render() {
+    return (
+      <div>
+        ThumbnailGallery
+      </div>
+    );
+  }
+}
